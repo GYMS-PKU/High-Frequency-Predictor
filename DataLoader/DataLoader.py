e47ce7228bcd412e220f3ef769a88d19f4c64146
GYMS-PKU <18316887409@163.com>
2022-01-01 10:25:00 +0800

    new DataLoader

--- DataLoader/DataLoader.py
+++ DataLoader/DataLoader.py
@@ -15,56 +15,18 @@ import pandas as pd
 import os
 
 
-class Data:
-    def __init__(self, code_order_dic: dict, order_code_dic: dict, date_position_dic: dict, position_date_dic: dict,
-                 data_dic: dict, spread: np.array, spread_dic: dict = None, top: np.array = None):
+class Data:  # 存放一只股票的所有交易日的日内高频数据
+    def __init__(self, data_dic: dict, ret: np.array):
         """
-        :param code_order_dic: 股票代码到矩阵位置的字典
-        :param order_code_dic: 矩阵位置到股票代码的字典
-        :param date_position_dic: 日期到矩阵下标的字典
-        :param data_dic: 所有的数据，形状一致
-        :param spread: 默认使用的价差
-        :param spread_dic: 可选的spread字典
-        :param top: 初始的top
+        :param data_dic: 数据字典，是一个
+        :param ret: 收益率
         """
-        self.code_order_dic = code_order_dic
-        self.order_code_dic = order_code_dic
-        self.date_position_dic = date_position_dic
-        self.position_date_dic = position_date_dic
         self.data_dic = data_dic
-        self.spread = spread
-        self.spread_dic = spread_dic
-        self.top = top
-
-    def get_real_date(self, start_date: str, end_date: str) -> (int, int):  # 用于获取起始日期对应的真正的数据起始位置
-        """
-        :param start_date: 任意输入的开始日期
-        :param end_date: 任意输入的结束日期
-        :return: 返回有交易的真正的起始日期对应的下标
-        """
-        tmp_start = start_date.split('-')
-        i = 0
-        while True:
-            s = datetime.date(int(tmp_start[0]), int(tmp_start[1]), int(tmp_start[2])) + datetime.timedelta(days=i)
-            try:
-                start = self.date_position_dic[s]
-                break
-            except KeyError:
-                i += 1
-        i = 0
-        tmp_end = end_date.split('-')
-        while True:
-            s = datetime.date(int(tmp_end[0]), int(tmp_end[1]), int(tmp_end[2])) + datetime.timedelta(days=i)
-            try:
-                end = self.date_position_dic[s]
-                break
-            except KeyError:
-                i -= 1
-        return start, end
+        self.ret = ret
 
 
 class DataLoader:
-    def __init__(self, data_path: str = 'D:/Documents/学习资料/DailyData/data',
+    def __init__(self, data_path: str = 'D:/Documents/学习资料/HFData',
                  back_test_data_path: str = 'D:/Documents/AutoFactoryData/BackTestData'):
         """
         :param data_path: 存放数据的路径
@@ -73,43 +35,47 @@ class DataLoader:
         self.data_path = data_path
         self.back_test_data_path = back_test_data_path
 
-    def load(self) -> Data:
+    def load(self, stock_num: int = 1, stock_list: list = None) -> dict:
         """
-        :return: data，包含data_dic，分别为OHLC四个价格；spread，价差
+        :param stock_num: 需要研究的股票数量
+        :param stock_list: 需要研究的股票列表
+        :return: dict，为所有股票的字典，值是Data
         """
-        days = os.listdir(self.data_path)
-        spread = np.zeros((len(days), 2081))  # 默认2081只股票
-        relative_spread = np.zeros((len(days), 2081))  # 默认2081只股票
-        names = ['open', 'low', 'high', 'close']  # 字段
-        code_order_dic = {}
-        order_code_dic = {}
-        date_position_dic = {}
-        position_date_dic = {}
-        data_dic = {name: np.zeros((len(days), 2081)) for name in names}
+        if stock_list is None:
+            stock_list = os.listdir('{}/data'.format(self.data_path))
+            stock_list = sorted(stock_list)
+            stock_list = stock_list[:stock_num]
+        datas = {}
 
-        codes = pd.read_csv('{}/{}'.format(self.data_path, days[0]))['Unnamed: 0'].values
-        for i in range(len(codes)):
-            stock_code = str(codes[i])
-            if len(stock_code) < 6:
-                stock_code = '0' * (6 - len(stock_code)) + stock_code
-            else:
-                stock_code = str(stock_code)
-            order_code_dic[i] = stock_code
-            code_order_dic[codes[i]] = i
-
-        day_num = 0
-        for day in days:
-            date_position_dic[datetime.date(2020, int(day[:2]), int(day[2:4]))] = day_num
-            position_date_dic[day_num] = datetime.date(2020, int(day[:2]), int(day[2:4]))
-            df = pd.read_csv('{}/{}'.format(self.data_path, day))
+        names = ['BidSize1', 'BidSize2', 'BidSize3', 'BidSize4', 'BidSize5', 'BidSize6', 'BidSize7',
+                 'BidSize8', 'BidSize9', 'BidSize10', 'BidPX1', 'BidPX2', 'BidPX3', 'BidPX4', 'BidPX5',
+                 'BidPX6', 'BidPX7', 'BidPX8', 'BidPX9', 'BidPX10', 'OfferSize1', 'OfferSize2', 'OfferSize3',
+                 'OfferSize4', 'OfferSize5', 'OfferSize6', 'OfferSize7', 'OfferSize8', 'OfferSize9', 'OfferSize10',
+                 'OfferPX1', 'OfferPX2', 'OfferPX3', 'OfferPX4', 'OfferPX5', 'OfferPX6', 'OfferPX7', 'OfferPX8',
+                 'OfferPX9', 'OfferPX10', 'mid_price', 'bid_side_depth_qty', 'offer_side_depth_qty', 'BidVol1',
+                 'OfferVol1', 'BidVol2', 'OfferVol2', 'BidVol3', 'OfferVol3', 'BidVol4', 'OfferVol4', 'BidVol5',
+                 'OfferVol5', 'BidVol6', 'OfferVol6', 'BidVol7', 'OfferVol7', 'BidVol8', 'OfferVol8', 'BidVol9',
+                 'OfferVol9', 'BidVol10', 'OfferVol10', 'bid_side_depth_vol', 'offer_side_depth_vol', 'quote_spread',
+                 'relative_quote_spread', 'wavg_spread_5', 'relative_wavg_spread_5', 'wavg_spread_10',
+                 'relative_wavg_spread_10', 'wavg_spread_20', 'relative_wavg_spread_20', 'wavg_spread_30',
+                 'relative_wavg_spread_30', 'wavg_spread_50', 'relative_wavg_spread_50', 'wavg_spread_75',
+                 'relative_wavg_spread_75', 'wavg_spread_100', 'relative_wavg_spread_100', 'wavg_spread_200',
+                 'relative_wavg_spread_200', 'wavg_spread_300', 'relative_wavg_spread_300', 'wavg_spread_400',
+                 'relative_wavg_spread_400', 'wavg_spread_500', 'relative_wavg_spread_500', 'OrderCount', 'OrderQty',
+                 'OrderVol', 'TradeCount', 'TradeQty', 'TradeVol']
+        for stock in stock_list:
+            stock_data = {name: [] for name in names}  # 最终得到一个days * snapshot_num的矩阵
+            ret = []
+            years = os.listdir('{}/data/{}'.format(self.data_path, stock))
+            for year in years:
+                days = os.listdir('{}/data/{}/{}'.format(self.data_path, stock, year))
+                for day in days:
+                    snapshot = pd.read_csv('{}/data/{}/{}/{}/snapshot.csv'.format(self.data_path, stock, year, day))
+                    ret.append(snapshot['ret'])  # 3s收益率
+                    for name in names:
+                        stock_data[name].append(snapshot[name].values)
+            ret = np.vstack(ret)
             for name in names:
-                data_dic[name][day_num] = df[name].values.copy()
-            spread[day_num] = df['bid_ask_spread'].values.copy()
-            relative_spread[day_num] = df['relative_spread'].values.copy()
-            day_num += 1
-
-        data = Data(code_order_dic=code_order_dic, order_code_dic=order_code_dic,
-                    position_date_dic=position_date_dic, date_position_dic=date_position_dic,
-                    spread=spread, spread_dic={'spread': spread, 'relative_spread': relative_spread},
-                    data_dic=data_dic)
-        return data
+                stock_data[name] = np.vstack(stock_data[name])
+            datas[stock] = Data(data_dic=stock_data, ret=ret)
+        return datas
